897674632b05527a458f9cbb4803c810d89e6809
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-07-25 15:29:40 +0300

    spawn new piece on collide

--- test_game.gd
+++ test_game.gd
@@ -1,12 +1,24 @@
 extends Node2D
 
+var piece_scene
+var last_piece: RigidBody2D
+
 func _ready():
-	pass
+	piece_scene = preload("res://objects/shapes/square.tscn")
+	_spawn_next_piece()
 
 func _process(delta):
-	var square = preload("res://objects/shapes/square.tscn")
-	var root = get_node(".")
-	var squareInstance = square.instantiate()
-	squareInstance.move_local_y(-250)
-	root.add_child(squareInstance)
 	pass
+
+func _spawn_next_piece():
+	if last_piece != null:
+		last_piece.body_entered.disconnect(_on_last_piece_collide)
+	last_piece = piece_scene.instantiate()
+	last_piece.contact_monitor = true
+	last_piece.max_contacts_reported = 1
+	last_piece.body_entered.connect(_on_last_piece_collide)
+	last_piece.move_local_y(-250)
+	get_node(".").add_child(last_piece)
+
+func _on_last_piece_collide(body):
+	_spawn_next_piece()
