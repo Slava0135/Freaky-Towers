28aadc3671fd0555478d78aee718211c5108bd70
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-07-25 15:01:04 +0300

    scale down

--- test_game.gd
+++ test_game.gd
@@ -7,6 +7,6 @@ func _process(delta):
 	var square = preload("res://objects/shapes/square.tscn")
 	var root = get_node(".")
 	var squareInstance = square.instantiate()
-	squareInstance.move_local_y(-5000)
+	squareInstance.move_local_y(-250)
 	root.add_child(squareInstance)
 	pass
