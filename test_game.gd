59c2a316e68a1903c1d65e17041c38dd7e0627c4
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-07-25 16:55:29 +0300

    random pieces

--- test_game.gd
+++ test_game.gd
@@ -4,11 +4,14 @@ const SIDE_STEP = 5
 const DROP_SPEED = 50
 const FAST_DROP_SPEED = 2 * DROP_SPEED
 
-var piece_scene
+var pieces: Array[PackedScene]
 var last_piece: RigidBody2D
 
 func _ready():
-	piece_scene = preload("res://objects/shapes/square.tscn")
+	pieces = [
+		preload("res://objects/pieces/square.tscn"),
+		preload("res://objects/pieces/long.tscn"),
+	]
 	_spawn_next_piece()
 
 func _process(delta):
@@ -28,7 +31,7 @@ func _spawn_next_piece():
 	if last_piece != null:
 		last_piece.freeze = false
 		last_piece.linear_velocity = Vector2.ZERO
-	last_piece = piece_scene.instantiate()
+	last_piece = pieces.pick_random().instantiate()
 	last_piece.move_local_y(-250)
 	last_piece.freeze = true
 	last_piece.freeze_mode = RigidBody2D.FREEZE_MODE_KINEMATIC
