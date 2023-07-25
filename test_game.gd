0672366fd4135be3b5c81ba0b99061a6621005dc
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-07-25 14:58:15 +0300

    spawn squares in scene

---
+++ test_game.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _ready():
+	pass
+
+func _process(delta):
+	var square = preload("res://objects/shapes/square.tscn")
+	var root = get_node(".")
+	var squareInstance = square.instantiate()
+	squareInstance.move_local_y(-5000)
+	root.add_child(squareInstance)
+	pass
