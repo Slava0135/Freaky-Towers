a1f4f74128283bccd681161e8b62de495246ec90
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-07-28 21:12:11 +0300

    exit game on esc when in main menu

--- interface/main_menu.gd
+++ interface/main_menu.gd
@@ -8,7 +8,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
 
 
 func _on_play_button_pressed():
