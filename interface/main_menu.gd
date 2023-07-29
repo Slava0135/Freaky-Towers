35f885028690dd531ddb62b02a0c7c335024b6ca
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-07-29 21:47:36 +0300

    use "ui_cancel"

--- interface/main_menu.gd
+++ interface/main_menu.gd
@@ -1,16 +1,11 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
-
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://games/survival.tscn")
--- interface/pause_button.gd
+++ interface/pause_button.gd
@@ -6,7 +6,7 @@ func _ready():
 	pass
 
 func _process(delta):
-	if Input.is_action_just_pressed("open_menu"):
+	if Input.is_action_just_pressed("ui_cancel"):
 		pause_game.emit()
 
 func _on_pressed():
